694efc48505cd2901fee7dcc1925dbc9426a5315
정해인 <junghaein@jeonghaein-ui-MacBookPro.local>
2025-11-28 15:31:05 +0900

    job table & profiles table & product tables & ideasGPT tables(before migrate)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./app/features/**/schema.ts",
-  out: "./app/sql/migrations",
+  out: "./app/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
