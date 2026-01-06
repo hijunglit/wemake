6756efca93a11956d939334f2f4d593e855ae049
정해인 <junghaein@jeonghaein-ui-MacBookPro.local>
2026-01-06 18:08:15 +0900

    login

--- app/features/auth/pages/login-page.tsx
+++ app/features/auth/pages/login-page.tsx
@@ -1,28 +1,59 @@
 import { Button } from "~/common/components/ui/button";
 import type { Route } from "./+types/login-page";
-import { Form, Link, useNavigation } from "react-router";
+import { Form, Link, redirect, useNavigation } from "react-router";
 import InputPair from "~/common/components/input-pair";
 import AuthButtons from "../components/auth-buttons";
 import { LoaderCircle } from "lucide-react";
+import z from "zod";
+import { makeSSRClient } from "~/supa-client";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Login | wemake" }];
 };
 
+const formSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email should be a string",
+    })
+    .email("Invalid email address"),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, { message: "Password must be at least 8 characters" }),
+});
+
 export const action = async ({ request }: Route.ActionArgs) => {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
   const formData = await request.formData();
-  // input 태그의 name 값 참조
-  const email = formData.get("email");
-  const password = formData.get("password");
-  return {
-    message: "success",
-  };
+  const { success, data, error } = formSchema.safeParse(
+    Object.fromEntries(formData)
+  );
+  if (!success) {
+    return {
+      formErrors: error.flatten().fieldErrors,
+    };
+  }
+  const { email, password } = data;
+  const { client, headers } = makeSSRClient(request);
+  const { error: loginError } = await client.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (loginError) {
+    return {
+      formErrors: null,
+      loginError: loginError.message,
+    };
+  }
+  return redirect("/", { headers });
 };
 
 export default function LoginPage({ actionData }: Route.ComponentProps) {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting =
+    navigation.state === "submitting" || navigation.state == "loading";
   return (
     <div className="flex flex-col relative items-center justify-center h-full">
       <Button variant={"ghost"} asChild className="absolute right-8 top-8">
@@ -40,6 +71,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
             type="email"
             placeholder="i.e wemake@gmail.com"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-sm text-red-500">
+              {actionData?.formErrors?.email?.join(", ")}
+            </p>
+          )}
           <InputPair
             id="password"
             label="Password"
@@ -49,6 +85,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
             type="password"
             placeholder="********"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-sm text-red-500">
+              {actionData?.formErrors?.password?.join(", ")}
+            </p>
+          )}
           <Button type="submit" className="w-full">
             {isSubmitting ? (
               <LoaderCircle className="animate-spin" />
@@ -56,8 +97,8 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
               "Log in"
             )}
           </Button>
-          {actionData?.message && (
-            <p className="text-sm text-red-500">{actionData.message}</p>
+          {actionData && "loginError" in actionData && (
+            <p className="text-sm text-red-500">{actionData.loginError}</p>
           )}
         </Form>
         <AuthButtons />
