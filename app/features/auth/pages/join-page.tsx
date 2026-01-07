1068554162f3b328ec22034bde8f7cda5cb66c01
정해인 <junghaein@jeonghaein-ui-MacBookPro.local>
2026-01-07 18:39:00 +0900

    sign up

--- app/features/auth/pages/join-page.tsx
+++ app/features/auth/pages/join-page.tsx
@@ -1,14 +1,63 @@
-import { Form, Link } from "react-router";
+import { Form, Link, redirect, useNavigation } from "react-router";
 import type { Route } from "./+types/join-page";
 import InputPair from "~/common/components/input-pair";
 import { Button } from "~/common/components/ui/button";
 import AuthButtons from "../components/auth-buttons";
+import { makeSSRClient } from "~/supa-client";
+import z from "zod";
+import { checkUsernameExists } from "../queries";
+import { LoaderCircle } from "lucide-react";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Join | wemake" }];
 };
 
-export default function JoinPage() {
+const formSchema = z.object({
+  name: z.string().min(3),
+  username: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export const action = async ({ request }: Route.ActionArgs) => {
+  const formData = await request.formData();
+  const { success, data, error } = formSchema.safeParse(
+    Object.fromEntries(formData)
+  );
+  if (!success) {
+    return {
+      formErrors: error.flatten().fieldErrors,
+    };
+  }
+  const usernameExists = await checkUsernameExists(request, {
+    username: data.username,
+  });
+  if (usernameExists) {
+    return { formErrors: { username: ["username already exists"] } };
+  }
+  const { client, headers } = makeSSRClient(request);
+  const { error: signUpError } = await client.auth.signUp({
+    email: data.email,
+    password: data.password,
+    options: {
+      data: {
+        name: data.name,
+        username: data.username,
+      },
+    },
+  });
+  if (signUpError) {
+    return {
+      signUpError: signUpError.message,
+    };
+  }
+  return redirect("/", { headers });
+};
+
+export default function JoinPage({ actionData }: Route.ComponentProps) {
+  const navigation = useNavigation();
+  const isSubmitting =
+    navigation.state === "submitting" || navigation.state === "loading";
   return (
     <div className="flex flex-col relative items-center justify-center h-full">
       <Button
@@ -20,7 +69,7 @@ export default function JoinPage() {
       </Button>
       <div className="flex flex-col items-center justify-center w-full max-w-md gap-10">
         <h1 className="text-2xl font-semibold">Join</h1>
-        <Form className="w-full space-y-5">
+        <Form className="w-full space-y-5" method="post">
           <InputPair
             id="name"
             label="Name"
@@ -30,6 +79,9 @@ export default function JoinPage() {
             type="text"
             placeholder="Enter your name"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData.formErrors?.name}</p>
+          )}
           <InputPair
             id="username"
             label="UserName"
@@ -40,6 +92,9 @@ export default function JoinPage() {
             type="text"
             placeholder="i.e wemake"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData.formErrors?.username}</p>
+          )}
           <InputPair
             id="email"
             label="Email"
@@ -49,6 +104,9 @@ export default function JoinPage() {
             type="email"
             placeholder="i.e wemake@gmail.com"
           />
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData.formErrors?.email}</p>
+          )}
           <InputPair
             id="password"
             label="Password"
@@ -58,9 +116,19 @@ export default function JoinPage() {
             type="password"
             placeholder="********"
           />
-          <Button type="submit" className="w-full">
-            Create Account
+          {actionData && "formErrors" in actionData && (
+            <p className="text-red-500">{actionData.formErrors?.password}</p>
+          )}
+          <Button type="submit" className="w-full" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              "Create account"
+            )}
           </Button>
+          {actionData && "signUpError" in actionData && (
+            <p className="text-red-500">{actionData.signUpError}</p>
+          )}
         </Form>
         <AuthButtons />
       </div>
